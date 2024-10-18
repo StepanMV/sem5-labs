0881645c1721168240449f6d9f5d3bc9343e6c79
StepanMV <mizyurinsv@gmail.com>
2024-10-18 11:20:37 +0300

    feat: single file deletion

--- archive/archive.c
+++ archive/archive.c
@@ -18,80 +18,14 @@ struct file_metadata
     gid_t gid;
 };
 
-void add_to_archive(const char *archive_name, const char *file_name)
+void delete_from_archive(const char *archive_name, const char *file_name)
 {
-    int archive_fd, file_fd;
-    struct stat file_stat;
-    struct file_metadata metadata;
-    char buffer[BUF_SIZE];
-    ssize_t bytes_read, bytes_written;
-
-    archive_fd = open(archive_name, O_WRONLY | O_CREAT | O_APPEND, 0644);
-    if (archive_fd == -1)
-    {
-        perror("Error opening archive");
-        exit(EXIT_FAILURE);
-    }
-
-    file_fd = open(file_name, O_RDONLY);
-    if (file_fd == -1)
-    {
-        perror("Error opening input file");
-        close(archive_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    if (fstat(file_fd, &file_stat) == -1)
-    {
-        perror("Error getting file stats");
-        close(file_fd);
-        close(archive_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    strncpy(metadata.filename, file_name, sizeof(metadata.filename) - 1);
-    metadata.size = file_stat.st_size;
-    metadata.mode = file_stat.st_mode;
-    metadata.uid = file_stat.st_uid;
-    metadata.gid = file_stat.st_gid;
-
-    bytes_written = write(archive_fd, &metadata, sizeof(metadata));
-    if (bytes_written != sizeof(metadata))
-    {
-        perror("Error writing metadata to archive");
-        close(file_fd);
-        close(archive_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    while ((bytes_read = read(file_fd, buffer, BUF_SIZE)) > 0)
-    {
-        bytes_written = write(archive_fd, buffer, bytes_read);
-        if (bytes_written != bytes_read)
-        {
-            perror("Error writing file data to archive");
-            close(file_fd);
-            close(archive_fd);
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    if (bytes_read == -1)
-    {
-        perror("Error reading file");
-    }
-
-    close(file_fd);
-    close(archive_fd);
-}
-
-void extract_from_archive(const char *archive_name)
-{
-    int archive_fd, file_fd;
+    int archive_fd, temp_fd;
     struct file_metadata metadata;
     char buffer[BUF_SIZE];
     ssize_t bytes_read, bytes_written;
     off_t file_size_remaining;
+    int file_found = 0;
 
     archive_fd = open(archive_name, O_RDONLY);
     if (archive_fd == -1)
@@ -100,94 +34,95 @@ void extract_from_archive(const char *archive_name)
         exit(EXIT_FAILURE);
     }
 
+    temp_fd = open("temp.arch", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (temp_fd == -1)
+    {
+        perror("Error opening temporary archive");
+        close(archive_fd);
+        exit(EXIT_FAILURE);
+    }
+
     while (read(archive_fd, &metadata, sizeof(metadata)) == sizeof(metadata))
     {
-        printf("Extracting file: %s\n", metadata.filename);
 
-        file_fd = open(metadata.filename, O_WRONLY | O_CREAT | O_TRUNC, metadata.mode);
-        if (file_fd == -1)
+        if (strcmp(metadata.filename, file_name) == 0)
         {
-            perror("Error creating output file");
-            close(archive_fd);
-            exit(EXIT_FAILURE);
+            printf("Deleting file: %s\n", metadata.filename);
+            file_found = 1;
+
+            if (lseek(archive_fd, metadata.size, SEEK_CUR) == (off_t)-1)
+            {
+                perror("Error seeking in archive");
+                close(archive_fd);
+                close(temp_fd);
+                exit(EXIT_FAILURE);
+            }
+
+            continue;
         }
 
-        if (fchown(file_fd, metadata.uid, metadata.gid) == -1)
+        bytes_written = write(temp_fd, &metadata, sizeof(metadata));
+        if (bytes_written != sizeof(metadata))
         {
-            perror("Error setting file ownership");
+            perror("Error writing metadata to temp archive");
+            close(archive_fd);
+            close(temp_fd);
+            exit(EXIT_FAILURE);
         }
 
         file_size_remaining = metadata.size;
         while (file_size_remaining > 0)
         {
-
             bytes_read = read(archive_fd, buffer, (file_size_remaining > BUF_SIZE) ? BUF_SIZE : file_size_remaining);
             if (bytes_read == -1)
             {
                 perror("Error reading from archive");
-                close(file_fd);
                 close(archive_fd);
+                close(temp_fd);
                 exit(EXIT_FAILURE);
             }
-
-            bytes_written = write(file_fd, buffer, bytes_read);
+            bytes_written = write(temp_fd, buffer, bytes_read);
             if (bytes_written != bytes_read)
             {
-                perror("Error writing to output file");
-                close(file_fd);
+                perror("Error writing to temp archive");
                 close(archive_fd);
+                close(temp_fd);
                 exit(EXIT_FAILURE);
             }
-
             file_size_remaining -= bytes_written;
         }
-
-        close(file_fd);
     }
 
     close(archive_fd);
-}
+    close(temp_fd);
 
-void display_archive_stats(const char *archive_name)
-{
-    int archive_fd;
-    struct file_metadata metadata;
-    int file_count = 0;
-    off_t total_size = 0;
-
-    archive_fd = open(archive_name, O_RDONLY);
-    if (archive_fd == -1)
+    if (!file_found)
     {
-        perror("Error opening archive");
-        exit(EXIT_FAILURE);
+        printf("File '%s' not found in the archive.\n", file_name);
+        remove("temp.arch");
     }
-
-    while (read(archive_fd, &metadata, sizeof(metadata)) == sizeof(metadata))
+    else
     {
-        file_count++;
-        total_size += metadata.size;
-        printf("File: %s, Size: %ld bytes, UID: %d, GID: %d, Mode: %o\n",
-               metadata.filename, metadata.size, metadata.uid, metadata.gid, metadata.mode);
 
-        if (lseek(archive_fd, metadata.size, SEEK_CUR) == (off_t)-1)
+        if (remove(archive_name) == -1)
         {
-            perror("Error seeking in archive");
-            close(archive_fd);
+            perror("Error removing original archive");
             exit(EXIT_FAILURE);
         }
+        if (rename("temp.arch", archive_name) == -1)
+        {
+            perror("Error renaming temp archive");
+            exit(EXIT_FAILURE);
+        }
+        printf("File '%s' deleted successfully from the archive.\n", file_name);
     }
-
-    printf("\nTotal files: %d\n", file_count);
-    printf("Total size: %ld bytes\n", total_size);
-
-    close(archive_fd);
 }
 
 int main(int argc, char *argv[])
 {
     if (argc < 3)
     {
-        fprintf(stderr, "Usage: %s -i|-e|-s archive_name [file_name]\n", argv[0]);
+        fprintf(stderr, "Usage: %s -i|-e|-s|-d archive_name [file_name]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
@@ -209,6 +144,15 @@ int main(int argc, char *argv[])
     else if (strcmp(argv[1], "-s") == 0)
     {
         display_archive_stats(archive_name);
+    }
+    else if (strcmp(argv[1], "-d") == 0)
+    {
+        if (argc != 4)
+        {
+            fprintf(stderr, "Usage: %s -d archive_name file_name\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+        delete_from_archive(archive_name, argv[3]);
     }
     else
     {
