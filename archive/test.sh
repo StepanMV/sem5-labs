c72cd1f72f7715f8c55708f94ea6f24103cf8c3e
StepanMV <mizyurinsv@gmail.com>
2024-10-18 09:44:39 +0300

    fix: naming

--- archive/test.sh
+++ archive/test.sh
@@ -5,20 +5,20 @@ echo "This is a test file 2" > file2.txt
 echo "This is a test file 3" > file3.txt
 ls *.txt *.arc
 
-./archiver test_archive.arc -a file1.txt
-./archiver test_archive.arc -a file2.txt
-./archiver test_archive.arc -a file3.txt
+./archive test_archive.arc -a file1.txt
+./archive test_archive.arc -a file2.txt
+./archive test_archive.arc -a file3.txt
 ls *.txt *.arc
 rm *.txt
 
-./archiver test_archive.arc -e
+./archive test_archive.arc -e
 ls *.txt *.arc
 rm *.txt
 
-./archiver test_archive.arc -d file2.txt
+./archive test_archive.arc -d file2.txt
 ls *.txt *.arc
 rm *.txt
 
-./archiver test_archive.arc -e
+./archive test_archive.arc -e
 ls *.txt *.arc
 rm *.txt
