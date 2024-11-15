346a150fd2187115b294ce06bccf64a28a602266
StepanMV <mizyurinsv@gmail.com>
2024-11-15 13:14:06 +0300

    fix: pipe?

--- pipe/pipe.c
+++ pipe/pipe.c
@@ -27,15 +27,13 @@ int main()
 
     if (pid > 0)
     {
-
-        close(pipe_fd[0]);
-
         parent_time = time(NULL);
         printf("Parent (PID: %d) time: %s", getpid(), ctime(&parent_time));
         snprintf(message, sizeof(message), "Parent (PID: %d) time: %s", getpid(), ctime(&parent_time));
 
-        sleep(5);
+        close(pipe_fd[0]);
 
+        sleep(5);
         write(pipe_fd[1], message, strlen(message) + 1);
 
         close(pipe_fd[1]);
@@ -46,12 +44,12 @@ int main()
         close(pipe_fd[1]);
 
         read(pipe_fd[0], message, sizeof(message));
+        
+        close(pipe_fd[0]);
 
         time_t child_time = time(NULL);
         printf("Child (PID: %d) time: %s", getpid(), ctime(&child_time));
         printf("RECV: %s\n", message);
-
-        close(pipe_fd[0]);
     }
 
     return 0;
